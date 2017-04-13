c55f195c698a8d5554254fc6880b9a5cbd455d3f
marcus-samsungsds <atplus12345@gmail.com>
2017-04-13 14:04:39 +0900

    Update moray-stat-query.d

--- dtrace/moray-stat-query.d
+++ dtrace/moray-stat-query.d
@@ -6,13 +6,19 @@
 
 moray*:::query-start
 {
-        starts[copyinstr(arg0)] = timestamp;
+  starts[copyinstr(arg0)] = timestamp;
 }
 
 moray*:::query-done
-/starts[copyinstr(arg0)] && (timestamp - starts[copyinstr(arg0)]) < 10000000 /
+/starts[copyinstr(arg0)] && (timestamp - starts[copyinstr(arg0)]) < 1000000/
 {
-        starts["10ms-under"]++;
+        starts["1ms-under"]++;
+}
+
+moray*:::query-done
+/starts[copyinstr(arg0)] && (timestamp - starts[copyinstr(arg0)]) >= 1000000 && (timestamp - starts[copyinstr(arg0)]) < 10000000 /
+{
+        starts["1ms-over"]++;
 }
 
 moray*:::query-done
@@ -23,10 +29,11 @@ moray*:::query-done
 
 profile:::tick-1sec
 {
-        printf("%d %d\n", starts["10ms-under"], starts["10ms-over"]
+        printf("%d %d %d\n", starts["1ms-under"], starts["1ms-over"], starts["10ms-over"]
         );
-                                starts["10ms-under"] = 0;
-                                starts["10ms-over"] = 0;
+        starts["1ms-under"] = 0;
+        starts["1ms-over"] = 0;
+        starts["10ms-over"] = 0;
 }
 
 profile:::tick-10sec
